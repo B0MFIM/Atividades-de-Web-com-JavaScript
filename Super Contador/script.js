7769b3c0e06ec3e6b39fde3d3f5a9ce8e70154fb
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-09-04 18:59:31 -0400

    Super Contador
    Site web desenvolvido com JavaScript para fornecer contagens aritméticas ao usuário.

---
+++ Super Contador/script.js
@@ -0,0 +1,34 @@
+function contar(){
+    let ini = document.getElementById("txti")
+    let fim = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById("res")
+
+    if (ini.lenght == 0 || fim.lenght == 0 || passo.lenght == 0){
+        res.innerHTML = "Impossível Contar"
+        //alert("Erro: FALTAM DADOS!")
+    } else {
+        res.innerHTML = "Contando: <br>"
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert("Passo Inválido! Considerando PASSO 1")
+            p = 1
+        }
+        if (i < f){
+            //Contagem Crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1f449} `
+            }
+            res.innerHTML += `\u{1f3c1}`
+        } else {
+            //Contagem Regressiva
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1f449} `
+            }
+            res.innerHTML += `\u{1f3c1}`
+        }
+        
+    }
+}
